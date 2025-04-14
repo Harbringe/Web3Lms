cc763d7ca740366dba98887a374511d9baeb81ef
Harbringe <aadityamehetre@icloud.com>
2025-04-14 23:58:55 +0530

    modified settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-l+8qy+mjk4a5hx=*g040%%jkfhep#e^0^-10pa0%qb!ucy_2b6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
