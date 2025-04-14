99edf45ab7170be7517e5ecbd4a09ee50777d28a
Harbringe <aadityamehetre@icloud.com>
2025-04-15 01:36:03 +0530

    .

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,13 +1,23 @@
 echo "Building backend..."
+
 echo "Installing dependencies..."
 pip3 install -r requirements.txt
+
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
-echo "Making build directory..."
+
+echo "Making static directories..."
 mkdir -p staticfiles_build
+
+echo "Copying static files to build directory..."
+cp -r templates/*  staticfiles_build/
+
 echo "Making migrations..."
 python3 manage.py makemigrations
+
 echo "Migrating database..."
 python3 manage.py migrate
+
 echo "Build completed."
+
 echo "Starting backend server..."
