eadf61e9db9ff7dfce3463dac3994faf1b1211fe
Harbringe <aadityamehetre@icloud.com>
2025-04-15 00:28:53 +0530

    modified build_files.sh

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,11 @@
+echo "Building backend..."
+echo "Installing dependencies..."
 pip install -r requirements.txt
-py manage.py collectstatic 
-py manage.py makemigrations
-py manage.py migrate
-py manage.py runserver
+echo "Making migrations..."
+python manage.py makemigrations
+echo "Migrating database..."
+python manage.py migrate
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+echo "Build completed."
+echo "Starting backend server..."
