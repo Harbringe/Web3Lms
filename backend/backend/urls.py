40105c00d929c8166864b111859ae7ee21685d35
Plant <aadityamehetre@gmail.com>
2025-04-16 18:56:42 +0530

    Update urls.py Swagger Schema

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -35,6 +35,8 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
+   url='https://web3lms.onrender.com',  
+   schemes=['https'],
 )
 
 urlpatterns = [
@@ -42,7 +44,7 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-     path("api/v1/", include("api.urls"))
+    path("api/v1/", include("api.urls"))
 ]
 
 
