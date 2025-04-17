17327ba83f2ad58b04d8d7ce3f477b6777b96a1b
Plant <aadityamehetre@gmail.com>
2025-04-17 17:56:48 +0530

    .

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -4,19 +4,7 @@ echo "Installing dependencies..."
 pip3 install -r requirements.txt
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
-
-echo "Making static directories..."
-mkdir -p staticfiles_build
-
-echo "Copying static files to build directory..."
-cp -r templates/*  staticfiles_build/
-
-echo "Making migrations..."
-python3 manage.py makemigrations
-
-echo "Migrating database..."
-python3 manage.py migrate
+python3 manage.py collectstatic --noinput 
 
 echo "Build completed."
 
