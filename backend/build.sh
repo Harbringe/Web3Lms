3c773636676302ebab7fbe757f4f291ec37c571a
Harbringe <aadityamehetre@icloud.com>
2025-04-24 01:13:25 +0530

    modified build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -10,4 +10,10 @@ echo "Creating superuser..."
 then python manage.py createsuperuser --no-input; 
 fi
 
+echo "Making migrations..."
+python manage.py makemigrations
+
+echo "Migrating..." 
+python manage.py migrate
+
 echo "Build Complete!"
