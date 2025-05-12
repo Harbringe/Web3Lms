597ce0736168d0857a17758e7fb8809b19406512
Harbringe <aadityamehetre@icloud.com>
2025-05-12 14:32:25 +0530

    modified user profile

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
@@ -25,7 +26,7 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.FileField(upload_to="user_folder", default="default-user.jpg", null=True, blank=True)
+    image = models.FileField(upload_to="user_folder", default=settings.DEFAULT_AVATAR, null=True, blank=True)
     full_name = models.CharField(max_length=100)
     country = models.CharField(max_length=100, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
@@ -39,14 +40,24 @@ class Profile(models.Model):
         
     
     def save(self, *args, **kwargs):
-        if self.full_name == "" or self.full_name == None:
-            self.full_name == self.user.username
+        # Sync user details to profile
+        if not self.full_name or self.full_name == "":
+            self.full_name = self.user.full_name or self.user.username
+        
+        # If user has a wallet address, ensure it's synced
+        if self.user.wallet_address and self.user.wallet_address != "None":
+            self.user.wallet_address = self.user.wallet_address
+            
         super(Profile, self).save(*args, **kwargs)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(
+            user=instance,
+            full_name=instance.full_name or instance.username,
+            image=settings.DEFAULT_AVATAR
+        )
 
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
