3a39614fb045d981608643ce384945ae7a47f2c9
Harbringe <aadityamehetre@icloud.com>
2025-05-12 14:22:39 +0530

    modified build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -4,15 +4,15 @@ pip install -r requirements.txt
 echo "Collecting Static Files..."
 python manage.py collectstatic --no-input 
 
-if [[ $CREATE_SUPERUSER ]]; 
-echo "Creating superuser..."
-then python manage.py createsuperuser --no-input; 
-fi
-
 echo "Making migrations..."
 python manage.py makemigrations
 
 echo "Migrating..." 
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]]; 
+echo "Creating superuser..."
+then python manage.py createsuperuser --no-input; 
+fi
 
 echo "Build Complete!"
