898105cfb2db919bca7650101bc85d4c34a464f9
Harbringe <aadityamehetre@icloud.com>
2025-05-12 14:02:35 +0530

    modified build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,6 @@ echo "Installing Dependencies..."
 pip install -r requirements.txt 
 
 echo "Collecting Static Files..."
-mkdir static 
 python manage.py collectstatic --no-input 
 
 if [[ $CREATE_SUPERUSER ]]; 
